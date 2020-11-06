a6ccaedcf39a79383ea49d9b5e93d22a9e186792
adrianaguu <abel.hancco@ucsp.edu.pe>
2020-11-05 21:10:38 -0500

    register

---
+++ clients/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime  
+
+
+
+class Client(models.Model):
+    user = models.OneToOneField(User,on_delete=models.SET_NULL,null=True)
+    address = models.CharField(max_length=128)
+    reference = models.CharField(max_length=128)
+    gender = models.CharField(max_length=20)
+    exercise = models.CharField(max_length=20)
+    birth = models.DateField()
+    weight = models.FloatField()
+    height = models.FloatField()
+    calories = models.IntegerField()
+    goal = models.CharField(max_length=30)
+    health_condition = models.CharField(max_length=30)
+
+class Membership(models.Model):
+    date = models.DateField(default=datetime.now)
+    client = models.OneToOneField(Client,on_delete=models.CASCADE)
+    period = models.CharField(max_length=20)
